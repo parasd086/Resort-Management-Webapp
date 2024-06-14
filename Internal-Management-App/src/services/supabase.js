222ac90ffbb7f4125e0a3cb264d948127bada2b9
parasd086 <parasd086@gmail.com>
2024-06-14 18:54:26 +0530

    IMA- Add filter and sort capabilities to Cabin and minor bug fix

--- Internal-Management-App/src/services/supabase.js
+++ Internal-Management-App/src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = "https://dekpawffkmzweucfblxj.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRla3Bhd2Zma216d2V1Y2ZibHhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc4MzkzNTMsImV4cCI6MjAzMzQxNTM1M30.PQcydcco7m2hsQSJU3DMXINzpVkcf86XB9J11wwpKC0";
+const supabaseKey = "xxxx";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
