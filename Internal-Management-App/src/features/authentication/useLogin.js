6048ef8e2d6ce22bd6c41c3538d4f544b0621849
parasd086 <parasd086@gmail.com>
2024-06-17 18:30:55 +0530

    IMA: Add authentication and authorization

---
+++ Internal-Management-App/src/features/authentication/useLogin.js
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
+
+export function useLogin() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (user) => {
+      // After successfull log-in the data(user) just have been setup/downloaded and "getCurrentUser" from apiAuth isn't required to run yet again for re-verification. So, we manually add this query so instead of fetching currentUser from DB it uses cache.
+      queryClient.setQueriesData(["user"], user);
+      navigate("/dashboard");
+    },
+    onError: (err) => {
+      console.log("Error", err);
+      toast.error("Provided email or password are incorrect");
+    },
+  });
+
+  return { login, isLoading };
+}
---
+++ Internal-Management-App/src/services/apiAuth.js
@@ -0,0 +1,27 @@
+import supabase from "./supabase";
+
+export async function login({ email, password }) {
+  //   console.log(email, password);
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) throw new Error(error.message);
+  //   console.log(data);
+  return data;
+}
+
+export async function getCurrentUser() {
+  //This will get data/session from localStorage
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser();
+
+  //   console.log(data);
+
+  if (error) throw new Error(error.message);
+
+  return data?.user;
+}
