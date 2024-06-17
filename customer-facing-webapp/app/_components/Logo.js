02e9cbe91fa1f99615e042461fd12ea672681550
parasd086 <parasd086@gmail.com>
2024-06-17 09:23:48 +0530

    CFW: Add navigation improvement, root layout fix and image optimizations

--- customer-facing-webapp/app/_components/Logo.js
+++ customer-facing-webapp/app/_components/Logo.js
@@ -1,9 +1,23 @@
+import Image from "next/image";
 import Link from "next/link";
+import logo from "@/public/logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <img src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
+      {/* The image comp. want us to prevent layout shift therefore height and width are compulsory */}
+      {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
+
+      {/* Dynamic Image Import(without the src attribute)-> import logo from 'public/logo.png'; and <Image src={logo} alt="Logo" /> -
+      This method allows Next.js to analyze the image first and automatically handle
+      dimensions. */}
+      <Image
+        src={logo}
+        quality={100}
+        height="60"
+        width="60"
+        alt="The Wild Oasis logo"
+      />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
--- customer-facing-webapp/app/layout.js
+++ customer-facing-webapp/app/layout.js
@@ -9,6 +9,7 @@ const josefin = Josefin_Sans({
 });
 
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 export const metadata = {
   // title: "The Wild Oasis",
@@ -16,6 +17,7 @@ export const metadata = {
     template: "%s- The Wild Oasis",
     default: "Welcome- The Wild Oasis",
   },
+  //For SEO
   description:
     "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
@@ -24,14 +26,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} bg-primary-950 antialiased text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer> Copyright by The Wild Oasis</footer>
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
