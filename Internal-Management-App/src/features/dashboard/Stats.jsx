fab6594dda91f432aadc018b38272ef830896783
parasd086 <parasd086@gmail.com>
2024-06-19 09:12:43 +0530

    IMA: Add Dashboard Layout and statistics for Dashboard

---
+++ Internal-Management-App/src/features/dashboard/Stats.jsx
@@ -0,0 +1,52 @@
+import { formatCurrency } from "../../utils/helpers";
+import Stat from "./Stat";
+import {
+  HiOutlineBanknotes,
+  HiOutlineBriefcase,
+  HiOutlineCalendarDays,
+  HiOutlineChartBar,
+} from "react-icons/hi2";
+
+function Stats({ bookings, confirmedStays, numDays, cabinCount }) {
+  //1.
+  const numBookings = bookings?.length;
+  //2. total sales
+  const sales = bookings?.reduce((acc, cur) => acc + cur.totalPrice, 0);
+  //3. Total check-ins
+  const checkin = confirmedStays?.length;
+  //4. occupancy rate = num of checkedin nights/ total nights(num-cabins * total-days)
+  const occupation =
+    confirmedStays?.reduce((acc, cur) => acc + cur.numNights, 0) /
+    (numDays * cabinCount);
+
+  return (
+    <>
+      <Stat
+        title="Bookings"
+        color="blue"
+        icon={<HiOutlineBriefcase />}
+        value={numBookings}
+      />
+      <Stat
+        title="Sales"
+        color="gree"
+        icon={<HiOutlineBanknotes />}
+        value={formatCurrency(sales)}
+      />
+      <Stat
+        title="Check ins"
+        color="indigo"
+        icon={<HiOutlineCalendarDays />}
+        value={checkin}
+      />
+      <Stat
+        title="Occupancy rate"
+        color="yellow"
+        icon={<HiOutlineChartBar />}
+        value={Math.round(occupation * 100) + "%"}
+      />
+    </>
+  );
+}
+
+export default Stats;
