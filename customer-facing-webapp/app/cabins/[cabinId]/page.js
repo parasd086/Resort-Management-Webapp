31d31bf02a342ece5cda60452fc45a432ca34162
parasd086 <parasd086@gmail.com>
2024-06-22 18:42:11 +0530

    CFW: Add TextExpander to cabin page

--- customer-facing-webapp/app/cabins/[cabinId]/page.js
+++ customer-facing-webapp/app/cabins/[cabinId]/page.js
@@ -1,10 +1,20 @@
-import { getCabin } from "@/app/_lib/data-service";
+import TextExpander from "@/app/_components/TextExpander";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
   return { title: `Cabin ${name}` };
+}
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  // We have to return array of objects and each object(key:value) here the value should be in string.
+  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+
+  return ids;
 }
 
 export default async function Page({ params }) {
@@ -30,7 +40,9 @@ export default async function Page({ params }) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
