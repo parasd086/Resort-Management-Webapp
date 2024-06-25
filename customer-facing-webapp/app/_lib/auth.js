8a05796beb00c40c363f084487fc9c618085a228
parasd086 <parasd086@gmail.com>
2024-06-25 13:37:41 +0530

    CFW: Add guest after first sign-in

--- customer-facing-webapp/app/_lib/auth.js
+++ customer-facing-webapp/app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -16,6 +17,29 @@ const authConfig = {
       //"!!" is a shorthand technique often used for converting a value to its equivalent boolean representation.
       //The first ! negates the value, and the second ! negates it again, effectively casting it to a boolean
       return !!auth?.user;
+    },
+    //So, it's a bit like middleware, if you think about it.  It happens after the user has put in their credentials,  but before they're actually  like really, really locked into the application.
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+
+        // has to return true to move-on with whole sign-in process.
+        return true;
+      } catch {
+        //if there's error in getGuest() it will not move on with whole sign-in process.
+        return false;
+      }
+    },
+    //and it runs after the sign in callback,  and also each time that the session is checked out.
+    //And so, here, we actually get access to the session,  and so this is the perfect place  to add the guest ID to that session.
+    async session() {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      //And then we need to return a session here,  otherwise, the whole thing will be broken.  Then we get no more session once we call off.
+      return session;
     },
   },
   pages: {
